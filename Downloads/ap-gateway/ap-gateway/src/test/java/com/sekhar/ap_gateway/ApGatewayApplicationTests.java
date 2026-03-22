75a291a9a2db38f4ca1677d2b15ea64befe2ced1
S Sekhar <ss2114699@gmail.com>
2026-03-22 11:21:55 +0530

    my java file

---
+++ Downloads/ap-gateway/ap-gateway/src/test/java/com/sekhar/ap_gateway/ApGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekhar.ap_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
